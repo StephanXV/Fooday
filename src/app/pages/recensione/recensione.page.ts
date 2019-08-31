52e15eb0b2e741092cdff9f665042f91ec5106db
Stefano Florio <31825478+StephanXV@users.noreply.github.com>
2019-08-31 11:25:07 +0200

    Review Templating

--- src/app/pages/recensione/recensione.page.ts
+++ src/app/pages/recensione/recensione.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-recensione',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecensionePage implements OnInit {
 
-  constructor() { }
+  private reviewFormModule: FormGroup;
+  numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+  constructor(private formBuilder: FormBuilder,
+              private navController: NavController) { }
 
   ngOnInit() {
+    this.reviewFormModule = this.formBuilder.group({
+      votoCucina: ['', Validators.compose([Validators.required])],
+      votoServizio: ['', Validators.compose([Validators.required])],
+      votoPrezzo: ['', Validators.compose([Validators.required])],
+      descrizione: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  onReviewSubmit(){
+    console.log('Recensione Pubblicata');
+    this.navController.navigateBack('tabs/prenotazioni');
   }
 
 }
