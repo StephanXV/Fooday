dde818bb7ca22ad0036ef4a5b74ac8ad7d3dbbb1
Stefano Florio <31825478+StephanXV@users.noreply.github.com>
2019-09-06 11:48:23 +0200

    Details Template

--- src/app/pages/dettagli-ristorante/dettagli-ristorante.page.ts
+++ src/app/pages/dettagli-ristorante/dettagli-ristorante.page.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DettagliRistorantePage implements OnInit {
   view: string;
+  antipasti = ['Bruschette', 'Arancini', 'Patatine fritte'];
+  primi = ['Spaghetti allo scoglio', 'Linguine all\'astice', 'Gnocchi al sugo'];
+  secondi = ['Cotoletta alla milanese', 'Arrosto di Maiale'];
+  dessert = ['Frutta di stagione', 'Torta al cioccolato', 'Cheesecake'];
+  bevande = ['Coca-Cola', 'Birra', 'Acqua', 'Sprite', 'Fanta'];
+  reviews = ['Tutto bene', 'Cibo ottimo, prezzo un po meno', 'Ci siamo sentiti a casa, davvero cordiali']
   constructor() { }
 
   ngOnInit() {
