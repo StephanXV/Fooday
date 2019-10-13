367ced7b3bfe6d4d61538e51d2fcebb04d622be6
Enrico Monte <enricomonte@MBP-di-Enrico.lan>
2019-10-13 14:24:52 +0200

    bug filtri, paginazione home

--- src/app/here-map/here-map.component.ts
+++ src/app/here-map/here-map.component.ts
@@ -49,4 +49,21 @@ export class HereMapComponent implements OnInit {
     const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
   }
 
+  /*addMarkersToMap(map) {
+    var parisMarker = new H.map.Marker({lat:48.8567, lng:2.3508});
+    map.addObject(parisMarker);
+
+    var romeMarker = new H.map.Marker({lat:41.9, lng: 12.5});
+    map.addObject(romeMarker);
+
+    var berlinMarker = new H.map.Marker({lat:52.5166, lng:13.3833});
+    map.addObject(berlinMarker);
+
+    var madridMarker = new H.map.Marker({lat:40.4, lng: -3.6833});
+    map.addObject(madridMarker);
+
+    var londonMarker = new H.map.Marker({lat:51.5008, lng:-0.1224});
+    map.addObject(londonMarker);
+  }*/
+
 }
